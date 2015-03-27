eaf2227d769f62219c195dcfc1af06d38645204f
= <=>
2015-03-27 17:39:53 -0400

    noah's commit! :)

---
+++ javascript/lobby.js
@@ -0,0 +1,101 @@
+var userinfo = "";
+var currGameIds = []; //updated on requests to server
+var currGameNames = [];
+var uid = ""; //crazy long UUID string
+
+
+function init() {
+    console.log("init");
+    $("#games").hide();
+}
+function requestGameListData() {
+    $.ajax({
+	type: "POST",
+	url: "/lobbyDATA",
+	dataType: "json",
+	contentType: 'application/json; charset=UTF-8',
+	data: JSON.stringify({gamelist_request: true}),
+	    success: function (data, textStatus, jqXHR) {
+
+	     }
+     });
+ }
+function sendLoginData() {
+    console.log("yoooo");
+     var userdata = {
+	 name: document.getElementById("name").value,
+	 password: document.getElementById("password").value
+     };
+     $.ajax({
+	 type: "POST",
+	 url: "/lobbyDATA",
+	 dataType: "json",
+	 contentType: 'application/json; charset=UTF-8',
+	 data: JSON.stringify({login: userdata}),
+	 success: function (data, textStatus, jqXHR) {
+	     if (data["authentication"] === true) {
+		 $("#login").hide();
+		 $("#games").show();
+		 $("#submitted").text("Welcome back " + userdata["name"]+"!");
+		 userinfo = data["userinfo"];
+		 currGameIds = data["gameIds"];
+		 currGameNames = data["gameNames"];
+		 uid = data["uid"];
+		 refreshGameTable(currGameIds,currGameNames);
+	     } else {
+		 $("#submitted").text("invalid username or password. try again.");
+	     }
+	}
+    });
+}
+
+function addGame() {
+    gameName = $("#newgamename").val();
+    if (gameName == "") {
+	$("#newgameerror").text("Please enter a name");
+	return;
+    }
+    var newGameData = {
+	uid: uid,
+	gameName: gameName,
+	request: "new_game"
+    };
+
+    $.ajax({
+	type: "POST",
+	url: "/lobbyDATA",
+	dataType: "json",
+	contentType: "application/json; charset=UTF-8",
+	data: JSON.stringify({gameData: newGameData}),
+	success: function (data, textStatus, jqXHR0 ) {
+	    gameId = data["gameId"];
+	    addToGameTable(gameId,gameName);
+	}
+    });
+}
+
+function addToGameTable(id,name) {
+    $("#gamesTable").prepend("<tr data-id=" + id + "><td>" + name + "</td></tr>");
+}
+
+//idList - uuids of currently active games
+//nameList - names corresponding to uuids
+function refreshGameTable(idList, nameList) {
+   $("#gamesTable").empty(); 
+   for (i = 0; i < idList.length; i++) {
+	addToGameTable(idList[i],nameList[i]);
+   }
+   if (idList.length == 0) { //there are no games right now..
+	
+   }
+
+}
+
+
+$(function() {
+    init();
+    $("#loginSubmit").click(function(event) {
+	event.preventDefault();
+	sendLoginData();
+    });
+});
--- src/server/LobbyHandler.java
+++ src/server/LobbyHandler.java
@@ -9,6 +9,8 @@ import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  *
@@ -18,6 +20,8 @@ public class LobbyHandler extends ResponseHandler {
 
     private GameHandler gamehandler;
     public static final String NEW_GAME_STRING = "new_game";
+    public static final String JOIN_GAME_STRING = "join_game";
+    public static final String GAME_DATA = "gameData";
     public static final String LOGIN_STRING = "login";
     public static final String NAME_STRING = "name";
     public static final String PWD_STRING = "password";
@@ -26,8 +30,17 @@ public class LobbyHandler extends ResponseHandler {
     public static final String USER_COOKIE = "user_cookie";
     public static final String REQ_GAMELIST_STRING = "gamelist_request";
 
+    private ArrayList<String> gameIdList;
+    private ArrayList<String> gameNameList;
+    private ArrayList<String> userIdList;
+    private ArrayList<String> userNameList;
+
     public LobbyHandler(GameHandler g) {
         this.gamehandler = g;
+	this.gameIdList = new ArrayList<String>();
+	this.gameNameList = new ArrayList<String>();
+	this.userIdList = new ArrayList<String>();
+	this.userNameList = new ArrayList<String>();
     }
 
     @Override
@@ -44,12 +57,43 @@ public class LobbyHandler extends ResponseHandler {
             JSONObject ret = new JSONObject();
             if (name.equals("Eli") && pwd.equals("b")) {
                 ret.put(AUTH_STRING, true);
+		Object[] gameIdArray = gameIdList.toArray();
+		Object[] gameNameArray = gameNameList.toArray();
+		System.out.println("SIze: " + gameIdArray.length);
+		for (int i = 0; i < gameIdArray.length; i++) {
+		   gameIdArray[i] = (String) gameIdArray[i]; 
+		   gameNameArray[i] = (String) gameNameArray[i];
+		}
+		ret.put("gameIds",gameIdArray);
+		ret.put("gameNames",gameNameArray);
+		String uuid = UUID.randomUUID().toString();
+		userIdList.add(uuid);
+		userNameList.add(name);
+		ret.put("uid",uuid);
             } else {
                 ret.put(AUTH_STRING,false);
             }
             this.sendJSON(ret, he);
             sent = true;
-        }
+        } else if (jsonMap.has(GAME_DATA)) {
+	   JSONObject gameData = jsonMap.getJSONObject(GAME_DATA); 
+	   String uid = gameData.getString("uid");
+	   String gameName = gameData.getString("gameName");
+	   String requestType = gameData.getString("request");
+
+	   JSONObject ret = new JSONObject();
+	   if (requestType.equals(NEW_GAME_STRING)) {
+		String gameId = UUID.randomUUID().toString();
+		gameIdList.add(gameId);
+		gameNameList.add(gameName);
+		ret.put("gameId",gameId);	
+	   } else if (requestType.equals(JOIN_GAME_STRING)) {
+		ret.put("gameState","you've joined a game!!");
+	   }
+	   System.out.println("lobbyHandler received: " + uid + ", " + gameName + ", " + requestType);
+	   this.sendJSON(ret, he);
+	   sent = true;
+	}
         if (!sent) this.sendJSON(new JSONObject(), he);
     }
 	
